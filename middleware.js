0733b386132a05605a1024fc291f963c10ca6435
cipdv <cip.devries@gmail.com>
2024-04-03 14:21:09 -0400

    middleware only updates session

--- middleware.js
+++ middleware.js
@@ -4,22 +4,6 @@ import { updateSession } from "@/lib/auth";
 export async function middleware(request) {
   console.log("middleware ran successfully");
 
-  const currentUser = request.cookies.get("session")?.value;
-
-  if (
-    currentUser &&
-    !request.nextUrl.pathname.startsWith("/dashboard")
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-
-  if (
-    !currentUser &&
-    request.nextUrl.pathname.startsWith("/dashboard")
-  ) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
-
   return await updateSession(request);
 }
 
