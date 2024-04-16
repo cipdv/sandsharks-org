a39342575e9fbc99b6f1c633be63ee0d022802e2
cipdv <cip.devries@gmail.com>
2024-04-16 12:36:22 -0400

    slight change to db connection

--- app/lib/db.js
+++ app/lib/db.js
@@ -12,16 +12,37 @@ if (!URI) {
 let client = new MongoClient(URI, options);
 let dbConnection;
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global._mongoClientPromise) {
-    global._mongoClientPromise = client.connect();
-  }
-  dbConnection = global._mongoClientPromise;
-} else {
-  dbConnection = client.connect();
+if (!global._mongoClientPromise) {
+  global._mongoClientPromise = client.connect();
 }
+dbConnection = global._mongoClientPromise;
 
 export default dbConnection;
+
+// import { MongoClient } from "mongodb";
+
+// const URI = process.env.MONGODB_URI;
+// const options = {};
+
+// if (!URI) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env.local"
+//   );
+// }
+
+// let client = new MongoClient(URI, options);
+// let dbConnection;
+
+// if (process.env.NODE_ENV !== "production") {
+//   if (!global._mongoClientPromise) {
+//     global._mongoClientPromise = client.connect();
+//   }
+//   dbConnection = global._mongoClientPromise;
+// } else {
+//   dbConnection = client.connect();
+// }
+
+// export default dbConnection;
 // "use server";
 
 // import { MongoClient } from "mongodb";
